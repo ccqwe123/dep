ff55ba63910a4b8d57a218214124d9df5e53d1e4
unknown <jrc.jay38@gmail.com>
2025-02-20 12:59:39 +0800

    fix earnings

--- depined.py
+++ depined.py
@@ -1,9 +1,11 @@
 import asyncio
 import json
 import random
+import logging
 import cloudscraper
 from loguru import logger
 import time
+import sys
 
 POLLING_INTERVAL = 90  # in seconds
 KEEP_ALIVE_INTERVAL = 3  # in seconds
@@ -11,8 +13,8 @@ CHECK_BROWSER_STATE_INTERVAL = 30  # in seconds
 API_URL = "https://api.depined.org/api/user/widget-connect"
 EARNINGS_URL = "https://api.depined.org/api/stats/epoch-earnings"
 
-token = input('Please Enter your TOKEN: ')
 connection_state = True
+access_token = None  # Define as global variable
 
 user_agent = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
@@ -23,11 +25,23 @@ random_user_agent = random.choice(user_agent)
 
 scraper = cloudscraper.create_scraper()
 
+def get_access_token():
+    try:
+        with open('token.txt', 'r') as file:
+            return file.read().strip()
+    except Exception as e:
+        print(f"Error reading access token from file: {e}")
+        return None
+
 def get_headers():
     """Generate headers for API requests."""
+    if not access_token:
+        logger.error("Access token is missing!")
+        sys.exit(1)
+
     return {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {access_token}",
         "X-Requested-With": "XMLHttpRequest",
         "User-Agent": random_user_agent,
         "Accept": "application/json, text/plain, */*",
@@ -84,7 +98,14 @@ async def check_browser_state():
         await asyncio.sleep(CHECK_BROWSER_STATE_INTERVAL)
 
 async def main():
+    global access_token
     logger.info("Splekenesis Depined Bot")
+    access_token = get_access_token()  # Set access_token globally
+
+    if not access_token:
+        logger.error("Failed to retrieve access token. Exiting...")
+        sys.exit(1)
+
     await asyncio.gather(
         poll_api(),
         keep_alive(),
